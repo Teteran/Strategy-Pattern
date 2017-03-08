3240e6fd98ba2de396d2557a2379f3d1918a61c9
Kacper Tetera <kacper3d3@gmail.com>
2017-03-08 22:28:30 +0100

    Dodanie klasy Leczenie ktora implementuje interfejs Praca

---
+++ src/Leczenie.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Kacper on 2017-03-08.
+ */
+public class  Leczenie implements Praca {
+
+
+    @Override
+    public void pracuj() {
+        System.out.print("Lecze !");
+    }
+}
