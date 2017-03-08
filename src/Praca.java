f1ac40bb75aadbb2a5bb17f3d2701d1eb6790cce
Kacper Tetera <kacper3d3@gmail.com>
2017-03-08 21:54:46 +0100

    Dodanie Modelu

---
+++ src/Praca.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Kacper on 2017-03-08.
+ */
+public interface Praca {
+}
---
+++ src/Pracownik.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Kacper on 2017-03-08.
+ */
+public class Pracownik {
+
+    String zawod;
+    Praca praca;
+    Transport transport;
+    Pracownik(String zawod){
+        this.zawod = zawod;
+    }//
+}
