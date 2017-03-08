90db0cff8dfdc6e1ce08ac2a4c08e98a53d10cdc
Kacper Tetera <kacper3d3@gmail.com>
2017-03-08 22:34:52 +0100

    Dodanie Pieczenia :)

---
+++ src/Piecz.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Kacper on 2017-03-08.
+ */
+public class Piecz implements Praca{
+    @Override
+    public void pracuj() {
+        System.out.print("Pieke chlebek :)");
+    }
+}
