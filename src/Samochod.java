60a6c289820605e79f7256441fafce9796b1d475
Kacper Tetera <kacper3d3@gmail.com>
2017-03-09 16:14:41 +0100

    Dodanie 2 srodków transportu

---
+++ src/Samochod.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Kacper on 2017-03-09.
+ */
+public class Samochod implements  Transport {
+
+
+    @Override
+    public void jedz() {
+        System.out.println("Jade Samochodem! ..");
+
+    }
+}
---
+++ src/Tramwaj.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Kacper on 2017-03-09.
+ */
+public class Tramwaj implements Transport {
+
+    @Override
+    public void jedz() {
+        System.out.println("Dojezdzam tramwajem :D");
+
+    }
+}
